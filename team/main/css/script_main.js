84880135cb7e15a4ae0eb75046aea1dc45836312
Sungho Park <nonckk@gmail.com>
2023-01-17 18:05:19 +0900

    23.01.27
    힘내자

--- team/main/css/script_main.js
+++ team/main/css/script_main.js
@@ -1,11 +1,26 @@
-$(function () {
-  $(function slide() {
-    $(".slide")
-      .stop()
-      .animate({ marginLeft: "100%" }, 1200, function () {
-        $(".slide li:first").appendTo(".slide");
-        $(".slide").css({ marginLeft: 0 });
-      });
-    setInterval(slide, 5000);
-  });
-});
+var i = 0;
+var w = window.innerWidth;
+
+function slide() {
+  i++;
+
+  if (i > $(".slide li:last").index()) {
+    i = 0;
+  }
+
+  $(".slide li")
+    .eq(i - 1)
+    .css({ zIndex: 3 })
+    .stop()
+    .animate({ left: w }, 700, function () {
+      $(".slide li")
+        .eq(i - 1)
+        .css({ zIndex: 1, left: 0 });
+    });
+  $(".slide li").eq(i).css({ zIndex: 2 });
+  $(".slide li")
+    .eq(i + 1)
+    .css({ zIndex: 1 });
+}
+
+setInterval(slide, 5000);
