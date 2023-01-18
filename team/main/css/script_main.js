9e5a803f32accefa3bb19a10eb50e684283721c3
Sungho Park <nonckk@gmail.com>
2023-01-18 18:29:42 +0900

    23.01.18
    진행중

--- team/main/css/script_main.js
+++ team/main/css/script_main.js
@@ -1,26 +1,45 @@
-var i = 0;
-var w = window.innerWidth;
+$(function () {
+  var i = 0;
+  var w = window.innerWidth;
 
-function slide() {
-  i++;
+  function slide() {
+    i++;
 
-  if (i > $(".slide li:last").index()) {
-    i = 0;
+    if (i > $(".slide li:last").index()) {
+      i = 0;
+    }
+
+    $(".slide li")
+      .eq(i - 1)
+      .css({ zIndex: 3 })
+      .stop()
+      .animate({ left: w }, 700, function () {
+        $(".slide li")
+          .eq(i - 1)
+          .css({ zIndex: 1, left: 0 });
+      });
+    $(".slide li").eq(i).css({ zIndex: 2 });
+    $(".slide li")
+      .eq(i + 1)
+      .css({ zIndex: 1 });
   }
 
-  $(".slide li")
-    .eq(i - 1)
-    .css({ zIndex: 3 })
-    .stop()
-    .animate({ left: w }, 700, function () {
-      $(".slide li")
-        .eq(i - 1)
-        .css({ zIndex: 1, left: 0 });
-    });
-  $(".slide li").eq(i).css({ zIndex: 2 });
-  $(".slide li")
-    .eq(i + 1)
-    .css({ zIndex: 1 });
-}
-
-setInterval(slide, 5000);
+  setInterval(slide, 5000);
+  
+  // Initialize Swiper
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
