b3a7aa136e75159c359379772db86e1fbc0638e4
Sungho Park <nonckk@gmail.com>
2023-01-30 18:15:49 +0900

    23.01.30
    백업

--- team/main/css/script_main.js
+++ team/main/css/script_main.js
@@ -78,5 +78,38 @@ $(function () {
   $(".category").mouseout(function () {
     $(this).find(".category_info").removeClass("on");
   });
-  
+
+  //텍스트 flow 효과
+
+  $(window).on("load", function () {
+    setFlowBanner();
+  });
+
+  function setFlowBanner() {
+    const $wrap = $(".flow_wrap");
+    const $list = $(".flow_wrap .flow_list");
+    let wrapWidth = $wrap.width();
+    let listWidth = $list.width();
+    const speed =200; //1초에 몇픽셀 이동하는지 설정
+
+    //리스트 복제
+    let $clone = $list.clone();
+    $wrap.append($clone);
+    flowBannerAct()
+
+    //배너 실행 함수
+    function flowBannerAct() {
+      //무한 반복을 위해 리스트를 복제 후 배너에 추가
+      if (listWidth < wrapWidth) {
+        const listCount = Math.ceil((wrapWidth * 2) / listWidth);
+        for (let i = 2; i < listCount; i++) {
+          $clone = $clone.clone();
+          $wrap.append($clone);
+        }
+      }
+      $wrap.find(".flow_list").css({
+        animation: `${listWidth / speed}s linear infinite flowRolling`,
+      });
+    }
+  }
 });
