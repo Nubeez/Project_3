d7f8b9ea136da2b38b93914d98fd7d4961a44f77
Nubeez <nonckk@gmail.com>
2023-02-02 01:25:41 +0900

    반응형
    ㅇㅋ

--- team/main/css/script_main.js
+++ team/main/css/script_main.js
@@ -62,10 +62,25 @@ $(function () {
   // Initialize Swiper
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
-    spaceBetween: 30,
+    spaceBetween: 10,
     slidesPerGroup: 1,
     loop: false,
     loopFillGroupWithBlank: true,
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1, //브라우저가 768보다 클 때
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 2, //브라우저가 1024보다 클 때
+        spaceBetween: 10,
+      },
+      1200: {
+        slidesPerView: 3, //브라우저가 1024보다 클 때
+        spaceBetween: 10,
+      },
+    },
     pagination: {
       //   el: ".swiper-pagination",
       clickable: true,
@@ -94,17 +109,37 @@ $(function () {
   function setFlowBanner() {
     const $wrap = $(".flow_wrap");
     const $list = $(".flow_wrap .flow_list");
-    let wrapWidth = $wrap.width();
-    let listWidth = $list.width();
-    const speed = 200; //1초에 몇픽셀 이동하는지 설정
+    let wrapWidth = ""; //$wrap의 가로 크기
+    let listWidth = ""; //$list의 가로 크기
+    const speed = 92; //1초에 몇픽셀 이동하는지 설정
 
     //리스트 복제
     let $clone = $list.clone();
     $wrap.append($clone);
     flowBannerAct();
 
+    //반응형 :: 디바이스가 변경 될 때마다 배너 롤링 초기화
+    let oldWChk =
+      window.innerWidth > 1279 ? "pc" : window.innerWidth > 767 ? "ta" : "mo";
+    $(window).on("resize", function () {
+      let newWChk =
+        window.innerWidth > 1279 ? "pc" : window.innerWidth > 767 ? "ta" : "mo";
+      if (newWChk != oldWChk) {
+        oldWChk = newWChk;
+        flowBannerAct();
+      }
+    });
+
     //배너 실행 함수
     function flowBannerAct() {
+      //배너 롤링 초기화
+      if (wrapWidth != "") {
+        $wrap.find(".flow_list").css({ animation: "none" });
+        $wrap.find(".flow_list").slice(2).remove();
+      }
+      wrapWidth = $wrap.width();
+      listWidth = $list.width();
+
       //무한 반복을 위해 리스트를 복제 후 배너에 추가
       if (listWidth < wrapWidth) {
         const listCount = Math.ceil((wrapWidth * 2) / listWidth);
